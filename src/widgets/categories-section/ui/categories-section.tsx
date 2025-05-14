690113c891f76550d72206717fdd0c55d5a7d6c3
Abdulaziz <abizlancer@gmail.com>
2025-05-14 18:37:24 +0500

    feat: section title components and category section component

---
+++ src/widgets/categories-section/ui/categories-section.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import SectionTitle from '../../section-title/ui/section-title';
+import { cn } from '@shared/lib/utils';
+import { CategoryCardLazy, ICategory } from '@/entitites/category';
+
+interface Props {
+  wrapperClass?: string;
+  categoriesList: ICategory[]
+  title: string
+  text?: string
+  link?: string
+}
+
+const CategoriesSection: React.FC<Props> = (props) => {
+  const {
+    title,
+    text,
+    link,
+    wrapperClass,
+    categoriesList
+  } = props;
+
+  return (
+    <section className={cn('py-14', wrapperClass)}>
+      <div className="container">
+        <SectionTitle title={title} text={text} link={link} />
+
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 gap-x-4'>
+          {categoriesList.map((category, key) => (
+            <CategoryCardLazy
+              key={key}
+              title={category.title}
+              link={category.link}
+              imgUrl={category.imgUrl}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CategoriesSection;
